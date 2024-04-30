247795eaec8e6959f275c091058d615dd3b74f04
SalmaYasserMoselhi <salmayasser816@gmail.com>
2024-04-30 17:23:54 +0300

    gradle

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\salma\StudioProjects\Flutter_Project"
+export "FLUTTER_ROOT=D:\flutter-sdk\flutter"
+export "FLUTTER_APPLICATION_PATH=D:\flutter_project"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
