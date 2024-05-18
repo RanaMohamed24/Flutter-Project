c6ccb2cf0e5c0962a3096c43610369fe875630d5
RanaMohamed24 <raanaamohamed695@gmail.com>
2024-05-19 01:08:06 +0300

    login_auth function change

--- lib/features/auth/login/controller/cubit/login_cubit.dart
+++ lib/features/auth/login/controller/cubit/login_cubit.dart
@@ -1,4 +1,7 @@
+import 'dart:developer';
+
 import 'package:bloc/bloc.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_project/features/Home.dart';
 
@@ -17,6 +20,22 @@ class LoginCubit extends Cubit<LoginState> {
         context,
         MaterialPageRoute(builder: (context) => const Home()),
       );
+    }
+  }
+
+  Future<void> LogIn(BuildContext context) async {
+    try {
+      final credential = await FirebaseAuth.instance.signInWithEmailAndPassword(
+        email: emailController.text.trim(),
+        password: passwordController.text.trim(),
+      );
+      Navigator.of(context).pushReplacementNamed('Home');
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'user-not-found') {
+        log('No user found for that email.');
+      } else if (e.code == 'wrong-password') {
+        log('Wrong password provided for that user.');
+      }
     }
   }
 }
