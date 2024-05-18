b46bce4efa7cb8acce02e613e6188793cc3e9e87
RanaMohamed24 <raanaamohamed695@gmail.com>
2024-05-19 01:09:01 +0300

    signup_auth function

--- lib/features/auth/registration/controller/cubit/registration_cubit.dart
+++ lib/features/auth/registration/controller/cubit/registration_cubit.dart
@@ -1,4 +1,8 @@
+import 'dart:developer';
+
 import 'package:bloc/bloc.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 part 'registration_state.dart';
@@ -9,13 +13,39 @@ class RegistrationCubit extends Cubit<RegistrationState> {
   TextEditingController lastNameController = TextEditingController();
   TextEditingController emailController = TextEditingController();
   TextEditingController passwordController = TextEditingController();
-  TextEditingController  confirmPasswordController = TextEditingController();
+  TextEditingController confirmPasswordController = TextEditingController();
 
   GlobalKey<FormState> formKey = GlobalKey<FormState>();
 
   void onPressedConfirmButton(BuildContext context) {
     if (formKey.currentState!.validate()) {
       Navigator.pushNamed(context, 'verfication');
+    }
+  }
+
+  Future<void> SignUp(BuildContext context) async {
+    try {
+      final UserCredential =
+          await FirebaseAuth.instance.createUserWithEmailAndPassword(
+        email: emailController.text.trim(),
+        password: passwordController.text.trim(),
+      );
+      FirebaseFirestore.instance
+          .collection("Users")
+          .doc(UserCredential.user!.email)
+          .set({
+                "username": firstNameController.text + " " + lastNameController.text,
+                "bio": "empty bio",
+      });
+      Navigator.of(context).pushReplacementNamed('Auth');
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'weak-password') {
+        log('The password provided is too weak.');
+      } else if (e.code == 'email-already-in-use') {
+        log('The account already exists for that email.');
+      }
+    } catch (e) {
+        log(e.toString());
     }
   }
 }
--- lib/features/auth/registration/view/components/registration_required_fields_widget.dart
+++ lib/features/auth/registration/view/components/registration_required_fields_widget.dart
@@ -164,32 +164,8 @@ class RegistrationRequiredFieldsWidget extends StatelessWidget {
                       style: ButtonStyle(
                           backgroundColor:
                               MaterialStateProperty.all(PrimaryColor)),
-                      onPressed: () async {
-                        try {
-                          final UserCredential = await FirebaseAuth.instance
-                              .createUserWithEmailAndPassword(
-                            email: controller.emailController.text.trim(),
-                            password: controller.passwordController.text.trim(),
-                          );
-                          FirebaseFirestore.instance
-                              .collection("Users")
-                              .doc(UserCredential.user!.email)
-                              .set({
-                                "username":controller.firstNameController.text+" "
-                                          +controller.lastNameController.text ,
-                                "bio" : "empty bio",
-                                
-                              });
-                          Navigator.of(context).pushReplacementNamed('Auth');
-                        } on FirebaseAuthException catch (e) {
-                          if (e.code == 'weak-password') {
-                            print('The password provided is too weak.');
-                          } else if (e.code == 'email-already-in-use') {
-                            print('The account already exists for that email.');
-                          }
-                        } catch (e) {
-                          print(e);
-                        }
+                      onPressed: () {
+                        controller.SignUp(context);
                       },
                       child: Text(
                         'Confirm',
